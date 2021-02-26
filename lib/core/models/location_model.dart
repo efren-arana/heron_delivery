5b7b343e04274b108d21ec9a553a3177d0a7ae75
Efren Arana <efren-f@hotmail.com>
2021-02-25 22:18:59 -0500

    Se agrego la clase modelo Location

---
+++ lib/core/models/location_model.dart
@@ -0,0 +1,9 @@
+
+class LocationModel {
+  final double _latitude;
+  final double _longitude;
+
+  LocationModel(this._latitude, this._longitude);
+  get latitude => _latitude;
+  get longitude => _longitude;
+}
